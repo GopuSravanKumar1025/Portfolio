69683e1f31d9d2cedca55d749f2dd09897efdc5c
GopuSravanKumar <sravan.7946@gmail.com>
2025-08-11 08:59:12 +0100

    Update Ai.py

--- VoiceAssistantDevelopment/Ai.py
+++ VoiceAssistantDevelopment/Ai.py
@@ -11,19 +11,19 @@ engine = pyttsx3.init()
 engine.setProperty('rate', 200) 
 
 def speak(text):
-    print(f"🗣️ Assistant: {text}")
+    print(f"Assistant: {text}")
     engine.say(text)
     engine.runAndWait()
 
 def listen_command():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        print("🎙️ Listening...")
+        print("Listening...")
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
     try:
         command = recognizer.recognize_google(audio)
-        print(f"👂 You said: {command}")
+        print(f"You said: {command}")
         return command.lower()
     except sr.UnknownValueError:
         speak("Sorry, I didn't catch that.")
